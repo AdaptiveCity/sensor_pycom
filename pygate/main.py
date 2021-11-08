9655812c8a5d222a08a0b7a70007e0090eca5ec9
rv355 <rv355@cam.ac.uk>
2021-11-08 16:51:47 +0000

    ethernet support for pygate

--- pygate/main.py
+++ pygate/main.py
@@ -1,9 +1,11 @@
-from network import WLAN
+from network import ETH, WLAN
 import time
 import ujson
 import machine
 from machine import RTC
 import pycom
+from get_time import get_current_time
+import socket
 
 print('\nStarting LoRaWAN concentrator')
 # Disable Hearbeat
@@ -25,30 +27,56 @@ def machine_cb (arg):
 # register callback function
 machine.callback(trigger = (machine.PYGATE_START_EVT | machine.PYGATE_STOP_EVT | machine.PYGATE_ERROR_EVT), handler=machine_cb)
 
-print('Loading Wifi config from config_wlan.json...')
-with open("config_wlan.json", "r") as wc:
-    wc = ujson.load(wc)
+# Try the Ethernet Connection first
+print('Trying to connect ethernet...',end='')
+timer = 0
+eth_flag = False
+try:
+   eth = ETH()
+   eth.init()
 
-print('Connecting '+wc["acp_id"]+' to WiFi ssid '+wc["ssid"]+'.',end='')
+   while not eth.isconnected():
+       print('.',end='')
+       time.sleep(1)
+       timer += 1
+       if timer > 10: break
 
-# Connect to a Wifi Network
-wlan = WLAN(mode=WLAN.STA)
-wlan.connect(ssid=wc["ssid"], auth=(WLAN.WPA2, wc["passphrase"]), hostname=wc["acp_id"])
+   if eth.isconnected():
+       print('Ethernet Connected OK')
+       print(eth.ifconfig())
+       eth_flag = True
+except:
+    print('Ethernet Exception')
 
-while not wlan.isconnected():
-    print('.', end='')
-    time.sleep(1)
-print(" OK\n")
+# Connect to WiFi if Ethernet not available
+if not eth_flag:
+    print('Ethernet Connection not available')
+    print('Loading Wifi config from config_wlan.json...')
+    with open("config_wlan.json", "r") as wc:
+        wc = ujson.load(wc)
 
-# Sync time via NTP server for GW timestamps on Events
-print('Syncing RTC via ntp...', end='')
+    print('Connecting '+wc["acp_id"]+' to WiFi ssid '+wc["ssid"]+'.',end='')
+
+    # Connect to a Wifi Network
+    wlan = WLAN(mode=WLAN.STA)
+    wlan.connect(ssid=wc["ssid"], auth=(WLAN.WPA2, wc["passphrase"]), hostname=wc["acp_id"])
+
+    while not wlan.isconnected():
+        print('.', end='')
+        time.sleep(1)    
+    print(wlan.ifconfig())
+    print("WLAN Connected OK\n")
+
+# Ensure that the interface is ready
+time.sleep(2)
+
+# Sync time using a socket connection to a local website
+print('Syncing RTC...', end='')
 rtc = RTC()
-rtc.ntp_sync(server="pool.ntp.org")
-
-while not rtc.synced():
-    print('.', end='')
-    time.sleep(.5)
-print(" OK\n")
+current_time = get_current_time()
+print('Web time.. ' + str(current_time))
+rtc.init(current_time)
+print('RTC OK.. ' + str(rtc.now()))
 
 # Read the GW config file from Filesystem
 with open('/flash/global_config.json','r') as fp:
